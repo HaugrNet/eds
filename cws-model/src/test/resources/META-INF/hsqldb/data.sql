29458c76aa1398f4415a35e99531c9fa36cf28c8
Kim Jensen <kim@dawn.dk>
2017-01-02 20:45:48 +0100

    #10: Switched DB Creation for testing from Entity Annotation generated to Script. It has several advantages to be able to generate the DB used via scripts, since the model is easier to control and various additional constraints can be added.

--- cws-model/src/test/resources/META-INF/hsqldb/data.sql
+++ cws-model/src/test/resources/META-INF/hsqldb/data.sql
@@ -5,6 +5,6 @@
 -- Initial Database Version is 1, initial Production CWS release is 1.0.0
 INSERT INTO versions(schema_version, cws_version) VALUES (1, '1.0.0');
 
--- Default, we have 1 Object Datatype, which is the folder. The rest is left to
+-- Default, we have 1 Object Type, which is the folder. The rest is left to
 -- the initial setup to create
-INSERT INTO datatypes (type_name, type_value) VALUES ('folder', 'Folder');
+INSERT INTO types (external_id, type_name, type_value) VALUES ('1aac6582-d85b-4e1c-9c34-a89d57ba4c02', 'folder', 'Folder');

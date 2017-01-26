17e6a40e530aca3338b992fd66288b81627b83b6
Kim Jensen <kim@dawn.dk>
2017-01-26 16:04:18 +0100

    #18: Added default admin account to the test data setup, and added a test to verify that we can create a new admin account.

--- cws-core/src/test/java/io/javadog/cws/core/services/SettingServiceTest.java
+++ cws-core/src/test/java/io/javadog/cws/core/services/SettingServiceTest.java
@@ -5,8 +5,11 @@ import io.javadog.cws.api.common.CredentialType;
 import io.javadog.cws.api.requests.SettingRequest;
 import io.javadog.cws.api.responses.SettingResponse;
 import io.javadog.cws.common.Settings;
+import io.javadog.cws.common.exceptions.CWSException;
 import io.javadog.cws.model.EntityManagerSetup;
 import org.junit.Test;
+
+import javax.persistence.Query;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,15 +21,49 @@ import static org.junit.Assert.assertThat;
 public final class SettingServiceTest extends EntityManagerSetup {
 
     @Test
+    public void testCreatingAdmin() {
+        // For most tests we need to have the Admin account present, so by
+        // default it already exists. So, to test that we actually *can* create
+        // the Admin Account, we first have to delete it from the DB, which
+        // we're doing here. Since each test is running within a transaction,
+        // and they are rolled back after completion, this should not disturb
+        // other tests.
+        final Query query = entityManager.createQuery("delete from MemberEntity");
+        query.executeUpdate();
+
+        final SettingService service = new SettingService(new Settings(), entityManager);
+        final SettingRequest request = prepareRequest();
+
+        final SettingResponse response = service.process(request);
+        assertThat(response.getReturnCode(), is(0));
+        assertThat(response.getReturnMessage(), is("Ok"));
+    }
+
+    @Test(expected = CWSException.class)
+    public void testNonAdminRequest() {
+        final SettingService service = new SettingService(new Settings(), entityManager);
+        final SettingRequest request = prepareRequest();
+        request.setName("not admin");
+
+        service.process(request);
+    }
+
+    @Test
     public void testInvokingRequest() {
         final SettingService service = new SettingService(new Settings(), entityManager);
+        final SettingRequest request = prepareRequest();
+
+        final SettingResponse response = service.process(request);
+        assertThat(response.getReturnCode(), is(0));
+        assertThat(response.getReturnMessage(), is("Ok"));
+    }
+
+    private static SettingRequest prepareRequest() {
         final SettingRequest request = new SettingRequest();
         request.setName(Constants.ADMIN_ACCOUNT);
         request.setCredentialType(CredentialType.PASSPHRASE);
         request.setCredential(Constants.ADMIN_ACCOUNT.toCharArray());
 
-        final SettingResponse response = service.process(request);
-        assertThat(response.getReturnCode(), is(0));
-        assertThat(response.getReturnMessage(), is("Ok"));
+        return request;
     }
 }
--- cws-model/src/test/resources/META-INF/hsqldb/data.sql
+++ cws-model/src/test/resources/META-INF/hsqldb/data.sql
@@ -78,3 +78,26 @@ INSERT INTO settings (name, setting, modifiable) VALUES ('cws.system.salt', 'Sys
 -- cause problems as they may not be read out safely again. So, please only
 -- change this if you are really sure.
 INSERT INTO settings (name, setting, modifiable) VALUES ('cws.system.charset', 'UTF-8', true);
+
+
+-- =============================================================================
+--  Following is TEST data, and should not be added in a PRODUCTION environment
+-- -----------------------------------------------------------------------------
+-- Unfortunately, JPA only allow setting 3 scripts when creating the database,
+-- the first is the actual model, which contain what is needed to setup the
+-- database, including all tables, views, procedures, constraints, etc. The
+-- second script is for the data (this one), but as we both need to have data
+-- for production and for testing, we're adding it all here. The final script
+-- is for destroying the database, which is needed of you have a real database
+-- and not just an in-memory database.
+-- =============================================================================
+
+-- Default Administrator User, it is set at the first request to the System, and
+-- is thus needed for loads of tests.
+insert into members (external_id, name, salt, public_key, private_key) values (
+    '483833a4-2af7-4d9d-953d-b1e86cac8035',
+    'admin',
+    '16dc08da-84f3-4ebb-af1d-c51d2c7c377d',
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvhrpCpflMJ5Z6+C6mbRQAPnLMcuqbQuoCC9c9eerFk4FYBJDLZ57KQ85t6vzTN8yRPd5cyx1t4knDncO3pptZ85fQRoJDN4FV1Q/3LhG9o6O0yYtnhUV8RCiBjqY6tYKBulwu78WzgMv+kms1/dDaEWelpWDkwj7IVdihJYA71bvAs2+6caggklHa53y3snIbJx/Tpoype6SgMV4vskKye55SJMMiX15dFgPBCvrprJVICQT0r4mhn4DZv1nVRfBKDO3B7kJkR3bBVkPl0tuvwRTrUeym2aWYYjoDemuLfAC1SynbdAF8Ezo6TTsio6DvMVgejnhIgvnZFsdPcKU8wIDAQAB',
+    'hGQFx4vVE/PCS6+VUw7NqKDHK7waRmQaCaV5CzULnpI5vD+acdRBsnZ9dZhrowEcERS2BwxGBePQYkg8pLLnfyFgJ1hK+IIOMHJrnTDVfZwZ3yUdipDVC1BnHmuu/IvhYcn/4pSUcRHYWJLMS0oSBwh0xXRMPWTVBZhkHSfXEAgKhIJlrLEUhF1/45y4QDeVLGhGCU9d0ddSFMsctuACI9ztdgnQph0P0Js4N3K0KRysJoQF4VAp0mHFUTAT1opjvcTb8H1mZwpn/VMoBXMhrY+WfNSCyAZrzyZRAbUgIm5l0uFe0oK3xmtCrPkw/17pf/9HhFBwb5W+a9Y80LpqFLjkkxZCc5thB895VRkOhsTbYCvV4GndgT5z50y1ziZN5IjeGDH+AZGZl+MzyA/2Q7cpEKgMbeh4aBjX8Z1SupAyrFZk64s1fKoIcpJ+rBWdD9nBILIYuGFeBubw2tC488OhRpt9hNflEQU2XM/lYQ60U7IVquz9WH88PXmhoDI6DE3qpt8S4HJTTRtQQADzs45O8I9QGEFOQsvo/XV6z4QcP435tXG4DV9vYZR8fI3V9FTpQyoF0qOUcKhwVxoYNt+Uo6Qb8HR0gCRPrUbe06bWDX7nCn2b1pQ6ydv5+kToCecUKmqg9VEMzwJfcUqbA7rDO1mh3gh/r+rmHPt0ddkEm1JFRxMyyEioZXM89NI4eAgOzx5KXkNQzDqX2nQ6dcp+jzKeVyEzUxrN4iwwEFdMaNBQrpCxitOKu9avqDQSg6VEARfs4b2UCvZIMVWHHXr9UvGCkWerhDzm4lZ4+bhf/knlHoMV5LYsrwW0XV2WvBdcnVa/7g9Ro0WlauoBbc5/8VCyBMdPObkkecudW7bRR0G6y5ZORWd5xXgbzC4d1vQcyz6I1iEdkIU/ufbGO0KS8cgneBk1Lo6XZCdarmb+Wa5UYU6DrsShgIev0KE/bLq62t1INoV7vAaJg+95gnZK3HBvlIHW8LMx3G1gJUPQVSf0TaLFlFdiFVwOXjJIow/S1LYhbexED75OTuxcb/QySgtp52Pcba8ob6unwilS5zcgimO/x4C5tClorieQEUpukqy2TojUyc7SJgI800oXvy81DbMgxUxO4yGq5BQI7KLbbltXDdX0iZR8Np4tcuPgC/lEHG7z2fivNdEmyC/oqul55IbE16KQT1ZyGXUCe/M5Mvbd853qxzmHj/p0QMt5OUEMTEVaT/mdiQibf7Beqbze5eVATXpEQmj3bwzEs9yRxNFu3aTZvLQoiwArmptPJDJJ0o0uMC2wnCNfZ4NMvDr6bKmE8W5LOLxUlXAgTv0MWqnqqfkRKW0yW5mLv2JW/yDAjVKZv1DBocsYs4hRp1LFgChoDfPLDCvEp/E48dhLc81mkczY5Cr4YFrKu2TF6WD8yD3JUCji6lxQr0UxoIQxYpX21sKvLUH4f4duJGqaCM8twy+i9O/kLapV30qQWs7cM0yUQ1aRu1ftVpBfdldbzWr1h4Ta8q55I3gU3ezxMKnJlP1iVLq5nC88nA2FBLmowKC1n41Mz4ZSh3SJ+SFMpDNZ8QwkmWIwM6D0J7k5WQrZ/Hext5+TMc6TD1f9ZUJV1JXBPJcPoiYQejWqXM4S3xeZ0bghYHE4OQg='
+);

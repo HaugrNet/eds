5c1e61a0ca765cbea93e4cb367518be544ff6894
Kim Jensen <kim@dawn.dk>
2017-09-15 12:06:43 +0200

    #21: Added first Signature Testing. Signature handling must be altered, so the Id is not required, but a fingerprint is instead used internally to identify the Signature Key.

--- cws-core/src/test/java/io/javadog/cws/core/services/SignatureServiceTest.java
+++ cws-core/src/test/java/io/javadog/cws/core/services/SignatureServiceTest.java
@@ -10,6 +10,13 @@ package io.javadog.cws.core.services;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import io.javadog.cws.api.common.ReturnCode;
+import io.javadog.cws.api.requests.FetchSignatureRequest;
+import io.javadog.cws.api.requests.SignRequest;
+import io.javadog.cws.api.requests.VerifyRequest;
+import io.javadog.cws.api.responses.FetchSignatureResponse;
+import io.javadog.cws.api.responses.SignResponse;
+import io.javadog.cws.api.responses.VerifyResponse;
 import io.javadog.cws.model.DatabaseSetup;
 import org.junit.Test;
 
@@ -22,10 +29,29 @@ import org.junit.Test;
  */
 public final class SignatureServiceTest extends DatabaseSetup {
 
-    // TODO test requesting data with a false root, i.e. a root for a different circle.
-
     @Test
-    public void testDummy() {
-        assertThat(Boolean.TRUE, is(true));
+    public void testSignVerifyFetch() {
+        final byte[] data = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890".getBytes(settings.getCharset());
+        final FetchSignatureService fetchService = new FetchSignatureService(settings, entityManager);
+        final VerifyService verifyService = new VerifyService(settings, entityManager);
+        final SignService signService = new SignService(settings, entityManager);
+
+        final SignRequest signRequest = prepareRequest(SignRequest.class, "member1");
+        signRequest.setData(data);
+        final SignResponse signResponse = signService.perform(signRequest);
+        assertThat(signResponse.getReturnCode(), is(ReturnCode.SUCCESS));
+
+        final VerifyRequest verifyRequest = prepareRequest(VerifyRequest.class, "member2");
+        verifyRequest.setData(data);
+        verifyRequest.setSignature(signResponse.getSignature());
+        verifyRequest.setSignatureId(signResponse.getSignatureId());
+        final VerifyResponse verifyResponse = verifyService.perform(verifyRequest);
+        assertThat(verifyResponse.getReturnCode(), is(ReturnCode.SUCCESS));
+
+        final FetchSignatureRequest fetchRequest = prepareRequest(FetchSignatureRequest.class, "member1");
+        final FetchSignatureResponse fetchResponse = fetchService.perform(fetchRequest);
+        assertThat(fetchResponse.getReturnCode(), is(ReturnCode.SUCCESS));
+        assertThat(fetchResponse.getSignatures().size(), is(1));
+        assertThat(fetchResponse.getSignatures().get(0).getVerifications(), is(1L));
     }
 }

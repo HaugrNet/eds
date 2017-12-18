0d54ff7f5a136377be4eb0a94ae81bc8d9a117dc
Kim Jensen <kim@dawn.dk>
2017-12-18 23:11:37 +0100

    Reduced log level for the JCE bug with destroying keys.

--- cws-core/src/main/java/io/javadog/cws/core/jce/CWSKey.java
+++ cws-core/src/main/java/io/javadog/cws/core/jce/CWSKey.java
@@ -75,6 +75,6 @@ public abstract class CWSKey<T extends Key> {
         //     - java.security.KeyPair should implement Destroyable
         //  o https://bugs.openjdk.java.net/browse/JDK-8160206
         //     - SecretKeySpec should implement destroy()
-        log.log(Settings.INFO, "Java support for destroying keys is not yet implemented.");
+        log.log(Settings.DEBUG, "Java support for destroying keys is not yet implemented.");
     }
 }

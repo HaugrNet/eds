0cb086c1199cfe1b61edd37cc028803e83607dd4
Kim Jensen <kim@dawn.dk>
2017-12-19 23:27:24 +0100

    #26: Corrections to the SOAP Test.

--- cws-client/src/test/java/io/javadog/cws/client/SoapClientTest.java
+++ cws-client/src/test/java/io/javadog/cws/client/SoapClientTest.java
@@ -8,11 +8,8 @@
 package io.javadog.cws.client;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import io.javadog.cws.api.Share;
 import io.javadog.cws.api.System;
 import io.javadog.cws.api.common.Action;
 import io.javadog.cws.api.common.Constants;
@@ -22,19 +19,14 @@ import io.javadog.cws.api.requests.FetchMemberRequest;
 import io.javadog.cws.api.requests.ProcessCircleRequest;
 import io.javadog.cws.api.requests.ProcessMemberRequest;
 import io.javadog.cws.api.requests.SettingRequest;
-import io.javadog.cws.api.requests.SignRequest;
-import io.javadog.cws.api.requests.VerifyRequest;
 import io.javadog.cws.api.responses.FetchCircleResponse;
 import io.javadog.cws.api.responses.FetchMemberResponse;
 import io.javadog.cws.api.responses.ProcessCircleResponse;
 import io.javadog.cws.api.responses.ProcessMemberResponse;
 import io.javadog.cws.api.responses.SettingResponse;
-import io.javadog.cws.api.responses.SignResponse;
-import io.javadog.cws.api.responses.VerifyResponse;
 import io.javadog.cws.api.responses.VersionResponse;
 import org.junit.Test;
 
-import java.nio.charset.Charset;
 import java.util.UUID;
 
 /**
@@ -43,11 +35,9 @@ import java.util.UUID;
  */
 public final class SoapClientTest {
 
-    private final System system = new SystemSoapClient(Base.URL);
-    private final Share share = new ShareSoapClient(Base.URL);
-
     @Test
     public void testVersion() {
+        final System system = new SystemSoapClient(Base.URL);
         final VersionResponse response = system.version();
         assertThat(response.getReturnCode(), is(ReturnCode.SUCCESS));
         assertThat(response.getReturnMessage(), is("Ok"));
@@ -56,6 +46,7 @@ public final class SoapClientTest {
 
     @Test
     public void testSettings() {
+        final System system = new SystemSoapClient(Base.URL);
         final SettingRequest request = Base.prepareRequest(SettingRequest.class, Constants.ADMIN_ACCOUNT);
 
         final SettingResponse response = system.settings(request);
@@ -65,6 +56,7 @@ public final class SoapClientTest {
 
     @Test
     public void testFetchMembers() {
+        final System system = new SystemSoapClient(Base.URL);
         final FetchMemberRequest request = Base.prepareRequest(FetchMemberRequest.class, Constants.ADMIN_ACCOUNT);
 
         final FetchMemberResponse response = system.fetchMembers(request);
@@ -76,6 +68,7 @@ public final class SoapClientTest {
     public void testProcessMembers() {
         final String accountName = UUID.randomUUID().toString();
 
+        final System system = new SystemSoapClient(Base.URL);
         final ProcessMemberRequest request = Base.prepareRequest(ProcessMemberRequest.class, Constants.ADMIN_ACCOUNT);
         request.setAction(Action.CREATE);
         request.setNewAccountName(accountName);
@@ -88,6 +81,7 @@ public final class SoapClientTest {
 
     @Test
     public void testFetchCircles() {
+        final System system = new SystemSoapClient(Base.URL);
         final FetchCircleRequest request = Base.prepareRequest(FetchCircleRequest.class, Constants.ADMIN_ACCOUNT);
 
         final FetchCircleResponse response = system.fetchCircles(request);
@@ -99,6 +93,7 @@ public final class SoapClientTest {
     public void testProcessCircles() {
         final String accountName = UUID.randomUUID().toString();
 
+        final System system = new SystemSoapClient(Base.URL);
         final ProcessMemberRequest memberRequest = Base.prepareRequest(ProcessMemberRequest.class, Constants.ADMIN_ACCOUNT);
         memberRequest.setAction(Action.CREATE);
         memberRequest.setNewAccountName(accountName);
@@ -115,23 +110,5 @@ public final class SoapClientTest {
         final ProcessCircleResponse response = system.processCircle(request);
         assertThat(response.getReturnMessage(), is("Ok"));
         assertThat(response.getReturnCode(), is(ReturnCode.SUCCESS));
-    }
-
-    @Test
-    public void testSigningVerify() {
-        final SignRequest signRequest = Base.prepareRequest(SignRequest.class, Constants.ADMIN_ACCOUNT);
-        final byte[] document = UUID.randomUUID().toString().getBytes(Charset.forName("UTF-8"));
-        signRequest.setData(document);
-        final SignResponse signResponse = share.sign(signRequest);
-        assertThat(signResponse.isOk(), is(true));
-        assertThat(signResponse.getSignature(), is(not(nullValue())));
-
-        final VerifyRequest verifyRequest = Base.prepareRequest(VerifyRequest.class, Constants.ADMIN_ACCOUNT);
-        verifyRequest.setData(document);
-        verifyRequest.setSignature(signResponse.getSignature());
-
-        final VerifyResponse verifyResponse = share.verify(verifyRequest);
-        assertThat(verifyResponse.isOk(), is(true));
-        assertThat(verifyResponse.isVerified(), is(true));
     }
 }

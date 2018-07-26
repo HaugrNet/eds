924448a0e54d9f33c4e7226052f1e64d14756973
Kim Jensen <kim@dawn.dk>
2018-07-26 17:08:59 +0200

    Created a new WildFly script, which is closer to something that can be used for installations. Also updated the Payara script to mimic the changes. Both scripts invoke the administrative scripts from the containers as far as possible. Also trying to add symlinks to the CWS War files, which will help with testing it.

--- accessories/release/bin/payara.sh
+++ accessories/release/bin/payara.sh
@@ -9,8 +9,6 @@ readonly dbPassword="cws"
 readonly dbHost="localhost"
 readonly dbName="cws"
 readonly domain="domain1"
-readonly cwsWar="`dirname $0`/../payara/cws.war"
-# Mapping the GLASSFISH_HOME to an internal variable, to have a shorthand for it
 readonly payara=${GLASSFISH_HOME}
 
 # Java & JBoss (Payara) settings
@@ -19,12 +17,41 @@ export JAVA_OPTS="${JAVA_OPTS} -Xms1303m -Xmx1303m -Djava.net.preferIPv4Stack=tr
 
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# Show Help - Displays the help, to use this script
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
+# Run the Control Script
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-show_help () {
+if [ "${payara}" = "" ]; then
+    echo "Script requires that the system variable \$GLASSFISH_HOME is defined."
+    echo
+    exit
+fi
+
+action=${1}
+if [ "${action}" = "configure" ]; then
+    netstat -ant | grep ":4848 " > /dev/null
+    if [ "$?" -ne "0" ]; then
+        echo "Starting Payara ..."
+        ${payara}/glassfish/bin/asadmin start-domain ${domain}
+    fi
+
+    echo "Configuring Payara for CWS"
+    ${payara}/glassfish/bin/asadmin add-library `dirname $0`/../postgresql-42.2.4.jar
+    ${payara}/glassfish/bin/asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.xa.PGXADataSource --restype javax.sql.XADataSource --property "User=${dbUser}:Password=${dbPassword}:URL=jdbc\:postgresql\://${dbHost}/${dbName}" cwsPool
+    ${payara}/glassfish/bin/asadmin create-jdbc-resource --connectionpoolid cwsPool datasources/cwsDS
+elif [ "${action}" = "start" ]; then
+    echo "Starting Payara ..."
+    ${payara}/glassfish/bin/asadmin start-domain ${domain}
+elif [ "${action}" = "stop" ]; then
+    echo "Stopping Payara ..."
+    ${payara}/glassfish/bin/asadmin stop-domain ${domain}
+elif [ "${action}" = "deploy" ]; then
+    echo "Deploying CWS"
+    ${payara}/glassfish/bin/asadmin deploy --force `dirname $0`/../payara/cws.war
+elif [ "${action}" = "undeploy" ]; then
+    echo "Undeploying CWS"
+    ${payara}/glassfish/bin/asadmin undeploy cws
+elif [ "${action}" = "log" ]; then
+    tail -f ${payara}/glassfish/domains/${domain}/logs/server.log
+else
     echo "Payara Control script"
     echo "Usage: `basename $0` [Action]"
     echo
@@ -36,108 +63,4 @@ show_help () {
     echo "    undeploy  Undeploy the currently deployed CWS snapshot"
     echo "    log       Tail on the Server Log"
     echo
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doConfigure - Attempts to configure Payara
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doConfigure () {
-    echo "Configuring running Payara instance for CWS"
-    netstat -ant | grep ":4848 " > /dev/null
-    if [ "$?" -ne "0" ]; then
-        doStart
-    fi
-    ${payara}/glassfish/bin/asadmin add-library `dirname $0`/../postgresql-42.2.4.jar
-    ${payara}/glassfish/bin/asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.xa.PGXADataSource --restype javax.sql.XADataSource --property "User=${dbUser}:Password=${dbPassword}:URL=jdbc\:postgresql\://${dbHost}/${dbName}" cwsPool
-    ${payara}/glassfish/bin/asadmin create-jdbc-resource --connectionpoolid cwsPool datasources/cwsDS
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doStart - Attempts to start Payara
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doStart () {
-    echo "Starting Payara ..."
-    ${payara}/glassfish/bin/asadmin start-domain ${domain}
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doStop - Attempts to stop Payara
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doStop () {
-    echo "Stopping Payara ..."
-    ${payara}/glassfish/bin/asadmin stop-domain ${domain}
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doDeploy - Attempts to deploy the latest snapshot to Payara
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doDeploy () {
-    echo "Deploying CWS"
-    ${payara}/glassfish/bin/asadmin deploy --force ${cwsWar}
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doUndeploy - Undeploy's current deployed snapshot from Payara
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doUndeploy () {
-    echo "Undeploying CWS"
-    ${payara}/glassfish/bin/asadmin undeploy cws
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# doLog - Tail on the Payara Server log
-# -----------------------------------------------------------------------------
-# Param  -> Void
-# Return -> Void
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-doLog () {
-    tail -f ${payara}/glassfish/domains/${domain}/logs/server.log
-}
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# Main part, please don't touch!
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-if [ -z "${payara}" ]; then
-    echo "Script requires that the system variable \$GLASSFISH_HOME is defined."
-    echo
-    exit
-fi
-
-action=${1}
-if [ "${action}" = "configure" ]; then
-    doConfigure
-elif [ "${action}" = "start" ]; then
-    doStart
-elif [ "${action}" = "stop" ]; then
-    doStop
-elif [ "${action}" = "deploy" ]; then
-    doDeploy
-elif [ "${action}" = "undeploy" ]; then
-    doUndeploy
-elif [ "${action}" = "log" ]; then
-    doLog
-else
-    show_help
 fi
---
+++ accessories/release/bin/wildfly.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# WildFly Control Script
+# -----------------------------------------------------------------------------
+# Configuration settings, please only modify this section
+# -----------------------------------------------------------------------------
+readonly dbUser="cws_user"
+readonly dbPassword="cws"
+readonly dbHost="localhost"
+readonly dbName="cws"
+readonly wildfly=${JBOSS_HOME}
+
+# Java & JBoss (WildFly) settings
+export JAVA_OPTS="${JAVA_OPTS} -Xms1303m -Xmx1303m -Djava.net.preferIPv4Stack=true"
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Run the Control Script
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+if [ "${wildfly}" = "" ]; then
+    echo "Script requires that the system variable \$JBOSS_HOME is defined."
+    echo
+    exit
+fi
+
+action=${1}
+if [ "${action}" = "configure" ]; then
+    netstat -ant | grep ":9990 " > /dev/null
+    if [ "$?" -ne "0" ]; then
+        echo "Starting WildFly ..."
+        ${wildfly}/bin/standalone.sh -Djboss.node.name=cws &
+        # The startup is not backgrounded automatically, it must be forced into
+        # the background, but we cannot configure WildFly until it is completed,
+        # so a simple sleep is added, not optimal - but it'll do the job
+        sleep 15
+    fi
+
+    echo "Configuring WildFly for CWS"
+    mkdir -p "${wildfly}/modules/org/postgresql/main"
+    cp `dirname $0`/../lib/postgresql-42.2.4.jar ${wildfly}/modules/org/postgresql/main
+    cp `dirname $0`/../wildfly/module.xml ${wildfly}/modules/org/postgresql/main
+    ${wildfly}/bin/jboss-cli.sh --connect --command="/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)" 2>/dev/null
+    ${wildfly}/bin/jboss-cli.sh --connect --command="data-source add --name=cwsDS --driver-name=postgresql --jndi-name=java:/datasources/cwsDS --connection-url=jdbc:postgresql://localhost:5432/cws --user-name=cws_user --password=cws --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true" 2>/dev/null
+    echo "WildFly has been configured"
+elif [ "${action}" = "start" ]; then
+    echo "Starting WildFly ..."
+    ${wildfly}/bin/standalone.sh -Djboss.node.name=cws &
+elif [ "${action}" = "stop" ]; then
+    echo "Stopping WildFly ..."
+    ${wildfly}/bin/jboss-cli.sh --connect --controller=localhost:9990 --command="shutdown"
+elif [ "${action}" = "deploy" ]; then
+    echo "Deploying CWS"
+    ${wildfly}/bin/jboss-cli.sh --connect --controller=localhost:9990 --command="deploy `dirname $0`/../wildfly/cws.war --force"
+elif [ "${action}" = "undeploy" ]; then
+    echo "Undeploying CWS"
+    ${wildfly}/bin/jboss-cli.sh --connect --controller=localhost:9990 --command="undeploy cws.war"
+elif [ "${action}" = "log" ]; then
+    tail -f ${wildfly}/standalone/log/server.log
+else
+    echo "WildFly Control script"
+    echo "Usage: `basename $0` [Action]"
+    echo
+    echo "  The Action must be one of the following:"
+    echo "    configure Attempts to configure a CWS WildFly instance"
+    echo "    start     Attempts to start a CWS WildFly instance"
+    echo "    stop      Attempts to stop the running CWS WildFly instance"
+    echo "    deploy    Deploy the latest CWS snapshot to WildFly"
+    echo "    undeploy  Undeploy the currently deployed CWS snapshot"
+    echo "    log       Tail on the Server Log"
+    echo
+fi

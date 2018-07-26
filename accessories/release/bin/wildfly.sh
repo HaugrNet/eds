76c2983e3b44706dcf2f492a408be3a60f4996dc
Kim Jensen <kim@dawn.dk>
2018-07-26 18:04:54 +0200

    Fixed sill typo, which caused the DB to not be correctly configured.

--- accessories/release/bin/wildfly.sh
+++ accessories/release/bin/wildfly.sh
@@ -49,7 +49,7 @@ if [ "${action}" = "configure" ]; then
     cp `dirname $0`/../lib/postgresql-42.2.4.jar ${wildfly}/modules/org/postgresql/main
     cp `dirname $0`/../wildfly/module.xml ${wildfly}/modules/org/postgresql/main
     ${wildfly}/bin/jboss-cli.sh --connect --command="/subsystem=datasources/jdbc-driver=postgresql:add(driver-name=postgresql,driver-module-name=org.postgresql,driver-xa-datasource-class-name=org.postgresql.xa.PGXADataSource)" 2>/dev/null
-    ${wildfly}/bin/jboss-cli.sh --connect --command="data-source add --name=cwsDS --driver-name=postgresql --jndi-name=java:/datasources/cwsDS --connection-url=jdbc:postgresql://${dbHost}:${dbPort}/${dbName} --user-name=${db_User} --password=${dbPassword} --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true" 2>/dev/null
+    ${wildfly}/bin/jboss-cli.sh --connect --command="data-source add --name=cwsDS --driver-name=postgresql --jndi-name=java:/datasources/cwsDS --connection-url=jdbc:postgresql://${dbHost}:${dbPort}/${dbName} --user-name=${dbUser} --password=${dbPassword} --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true" 2>/dev/null
     echo "WildFly has been configured"
 elif [ "${action}" = "start" ]; then
     echo "Starting WildFly ..."

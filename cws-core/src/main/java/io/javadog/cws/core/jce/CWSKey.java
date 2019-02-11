4aa0792561a89179816eaab93f3e9732e07d0673
Kim Jensen <kim@dawn.dk>
2019-02-11 23:32:22 +0100

    Adding a work-around to destroy Symmetric Keys, since Oracle have decided to mark the open tickets as "won't fix". The destruction of Symmetric Keys is done using Reflection, with the reasoning that it is better to have a bad hack than no security. The code has been made as error prone as possible as Key destruction should just work.

--- cws-core/src/main/java/io/javadog/cws/core/jce/CWSKey.java
+++ cws-core/src/main/java/io/javadog/cws/core/jce/CWSKey.java
@@ -18,10 +18,11 @@ package io.javadog.cws.core.jce;
 
 import io.javadog.cws.core.enums.KeyAlgorithm;
 import io.javadog.cws.core.model.Settings;
+
+import java.lang.reflect.Field;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.logging.Logger;
-import javax.security.auth.DestroyFailedException;
-import javax.security.auth.Destroyable;
 
 /**
  * <p>Common CWS Key, used for all crypto operations.</p>
@@ -64,29 +65,97 @@ public abstract class CWSKey<T extends Key> {
     }
 
     /**
-     * <p>The Secret &amp; Private Keys both extend the {@link Destroyable }
-     * interface, which means that they in theory can be destroyed. However,
-     * many of the implementations only have the default behaviour, which is to
-     * simply throw a {@link DestroyFailedException }. Hence this method will
-     * simply ignore this exception by logging it as a debug message. Hopefully
-     * the debug logs will be reduced in the future, once the implementation has
-     * been added.</p>
+     * <p>JCE provides an interface for the Keys, so it is possible to destroy
+     * them, see {@link javax.security.auth.Destroyable#destroy()}. However,
+     * not all Keys implement this, meaning that they will revert to throwing
+     * a {@link javax.security.auth.DestroyFailedException }. Meaning, that
+     * the keys used in CWS won't be destroyed as expected.</p>
      *
-     * <p>Unfortunately, it seems that the functionality is not implemented in
-     * Java 8, so to avoid large stacktrace in the logs, the code to destroy
-     * the keys is revoked and listed as pending..</p>
+     * <p>The issues with this has been addressed in the following tickets, but
+     * with no resolution other than &quot;won't fix&quot;:</p>
+     *
+     * <ul>
+     *   <li>https://bugs.openjdk.java.net/browse/JDK-8008795</li>
+     *   <li>https://bugs.openjdk.java.net/browse/JDK-8158689</li>
+     *   <li>https://bugs.openjdk.java.net/browse/JDK-8160206</li>
+     * </ul>
+     *
+     * <p>The reason for not implementing these seems to be that it is not
+     * possible to guarantee that the keys are completely destroyed since the
+     * memory containing them may have been swapped to disc. However, for CWS,
+     * the keys only live for the duration of a request, so this is not a
+     * problem.</p>
+     *
+     * <p>To resolve this problem in CWS, a rather bad hack using Reflection
+     * has been applied, meaning that this method will use Reflection to scan
+     * through a Class, and all instances of a byte array will be replaced with
+     * a new nullified byte array.</p>
+     *
+     * <p>Keys use either BigInteger (RSA Keys) or byte arrays (Symmetric Keys),
+     * so destroying them is not trivial. Since BigInteger have been designed
+     * as Immutable Objects, they are harder to destroy, whereas simple byte
+     * arrays are easier to destroy, so this logic will only destroy those.</p>
+     *
+     * <p>It should be noted, that Oracle have made some changes to how the
+     * Reflection API works in Java, so this code has been tested with Java
+     * version 8 &amp; 11. But, as the changes in the Reflection API may come
+     * soon, there is no guarantee that it will work in later versions of
+     * Java.</p>
+     *
+     * <p>One more note, since the code here is suppose to just work, i.e. be
+     * as error prone as possible, any Exception thrown here is simply caught
+     * and logged as a debug statement.</p>
      */
-    protected static void destroyKey() {
-        // From the OpenJDK bugs: (pending feature implementation)
-        //  o https://bugs.openjdk.java.net/browse/JDK-6263419
-        //     - No way to clean the memory for a java.security.Key
-        //     - Bug is closed, but comments are interesting
-        //  o https://bugs.openjdk.java.net/browse/JDK-8008795
-        //     - Clean memory in JCE implementations of private key and secret key
-        //  o https://bugs.openjdk.java.net/browse/JDK-8158689
-        //     - java.security.KeyPair should implement Destroyable
-        //  o https://bugs.openjdk.java.net/browse/JDK-8160206
-        //     - SecretKeySpec should implement destroy()
-        LOG.log(Settings.DEBUG, "Java support for destroying keys is not yet implemented.");
+    protected void destroyKey() {
+        try {
+            final Field[] fields = key.getClass().getDeclaredFields();
+            for (final Field field : fields) {
+                // The type is a class instance, but it is not possible to
+                // compare a class type directly using instanceof, when the
+                // expected type is a byte array, hence this little trick
+                // with comparing.
+                if (field.getType() == byte[].class) {
+                    // To preserve the original definition of the Class, the
+                    // original access information is saved, so it can be used
+                    // once we have done the hacking of the Field
+                    final boolean accessible = field.isAccessible();
+
+                    // Hacking the field, requires that it is accessible
+                    setAccessible(field, true);
+
+                    // First, read out the content of the field from the Object
+                    final byte[] bytes = (byte[]) field.get(key);
+                    // then, fill it with zeros
+                    Arrays.fill(bytes, (byte) 0);
+                    // and set it again
+                    field.set(key, bytes);
+
+                    // Hacking completed, restoring the old Access information
+                    setAccessible(field, accessible);
+                }
+            }
+        } catch (IllegalAccessException | SecurityException e) {
+            // This should never happen, but - if so, just log and ignore, at
+            // this level, CWS should be as error prone as possible
+            LOG.log(Settings.DEBUG, "Unable to delete Key: " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * <p>The method is wrapping the setAccessible method in the Java 8
+     * Reflection API, as it has seen a rather substantial semantically change
+     * in Java 9+. To minimize the number of annoying warnings to a minimum,
+     * this method will simply wrap the call to the Java 8 setAccessible
+     * method.</p>
+     *
+     * <p>The name of the method has not changed between Java 8 &amp; 9.
+     * Although CWS works with Java 9+, it is not planned to migrate the
+     * Language Level dependency until CWS version 2.0.</p>
+     *
+     * @param field      The field to set the Access Flag for
+     * @param accessible The Access Flag to set
+     */
+    private static void setAccessible(final Field field, final boolean accessible) {
+        field.setAccessible(accessible);
     }
 }

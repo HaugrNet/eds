689844466a94ab6acf72974b3b1e19ab35109f4b
Kim Jensen <kim@dawn.dk>
2019-06-23 20:40:53 +0200

    Tinkered a bit with the Docker files, so it also works without a proxy present.

--- accessories/docker.sh
+++ accessories/docker.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 readonly tmp_proxy=${http_proxy//http:\/\//}
-readonly bare_proxy=${tmp_proxy//\//}
+readonly proxy=${tmp_proxy//\//}
 
-if [[ "${bare_proxy}" != "" ]]; then
-    echo "Acquire::http::Proxy \"http://${bare_proxy}/\";" > docker/apt.conf
+if [[ "${proxy}" != "" ]]; then
+    echo "Acquire::http::Proxy \"http://${proxy}/\";" > docker/apt.conf
     echo "use_proxy = on" > docker/wgetrc
-    echo "http_proxy = ${http_proxy}" >> docker/wgetrc
-    echo "https_proxy = ${http_proxy}" >> docker/wgetrc
+    echo "http_proxy = http://${proxy}/" >> docker/wgetrc
+    echo "https_proxy = http://${proxy}/" >> docker/wgetrc
 else
     echo "" > docker/apt.conf
     echo "use_proxy = off" > docker/wgetrc
 fi
 
-docker build --build-arg HTTP_PROXY="${bare_proxy}" --build-arg HTTPS_PROXY="${bare_proxy}" -t cws1 -f docker/Dockerfile .
+cp ../cws-wildfly/target/cws.war docker
+docker build --build-arg HTTP_PROXY="${proxy}" --build-arg HTTPS_PROXY="${proxy}" -t cws -f docker/Dockerfile .
+rm docker/apt.conf docker/wgetrc docker/cws.war
+
+# Run as follows:
+# $ docker run -d -p 8080:8080 cws

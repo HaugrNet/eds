4b41ae864b72eed23376602bd7c33d624b99157d
Kim Jensen <kim@dawn.dk>
2026-01-10 10:51:05 +0100

    Added health checks

---
+++ eds-quarkus/src/main/java/net/haugr/eds/quarkus/EDSHealthCheck.java
@@ -0,0 +1,63 @@
+/*
+ * EDS, Encrypted Data Share - open source Cryptographic Sharing system.
+ * Copyright (c) 2016-2026, haugr.net
+ * mailto: eds AT haugr DOT net
+ *
+ * EDS is free software; you can redistribute it and/or modify it under the
+ * terms of the Apache License, as published by the Apache Software Foundation.
+ *
+ * EDS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the Apache License for more details.
+ *
+ * You should have received a copy of the Apache License, version 2, along with
+ * this program; If not, you can download a copy of the License
+ * here: https://www.apache.org/licenses/
+ */
+package net.haugr.eds.quarkus;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import net.haugr.eds.core.model.Settings;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+/**
+ * <p>MicroProfile Health Check for EDS on Quarkus. Reports the readiness status
+ * based on whether the EDS system has been properly initialized (database schema
+ * verified, settings loaded).</p>
+ *
+ * <p>This check is used for Kubernetes readiness probes. Liveness is handled
+ * by the default Quarkus liveness check.</p>
+ *
+ * @author Kim Jensen
+ * @since EDS 2.0
+ */
+@Readiness
+@ApplicationScoped
+public class EDSHealthCheck implements HealthCheck {
+
+    private final Settings settings;
+
+    public EDSHealthCheck() {
+        this.settings = Settings.getInstance();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HealthCheckResponse call() {
+        if (settings.isReady()) {
+            return HealthCheckResponse.named("EDS")
+                    .up()
+                    .withData("status", "EDS is ready")
+                    .build();
+        } else {
+            return HealthCheckResponse.named("EDS")
+                    .down()
+                    .withData("status", "EDS is not ready - database may not be initialized")
+                    .build();
+        }
+    }
+}
---
+++ eds-spring/src/main/java/net/haugr/eds/spring/EDSHealthIndicator.java
@@ -0,0 +1,56 @@
+/*
+ * EDS, Encrypted Data Share - open source Cryptographic Sharing system.
+ * Copyright (c) 2016-2026, haugr.net
+ * mailto: eds AT haugr DOT net
+ *
+ * EDS is free software; you can redistribute it and/or modify it under the
+ * terms of the Apache License, as published by the Apache Software Foundation.
+ *
+ * EDS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the Apache License for more details.
+ *
+ * You should have received a copy of the Apache License, version 2, along with
+ * this program; If not, you can download a copy of the License
+ * here: https://www.apache.org/licenses/
+ */
+package net.haugr.eds.spring;
+
+import net.haugr.eds.core.model.Settings;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>Spring Boot Health Indicator for EDS. Reports the health status based on
+ * whether the EDS system has been properly initialized (database schema
+ * verified, settings loaded).</p>
+ *
+ * @author Kim Jensen
+ * @since EDS 2.0
+ */
+@Component("edsHealthIndicator")
+public class EDSHealthIndicator implements HealthIndicator {
+
+    private final Settings settings;
+
+    public EDSHealthIndicator() {
+        this.settings = Settings.getInstance();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Health health() {
+        if (settings.isReady()) {
+            return Health.up()
+                    .withDetail("status", "EDS is ready")
+                    .build();
+        } else {
+            return Health.down()
+                    .withDetail("status", "EDS is not ready - database may not be initialized")
+                    .build();
+        }
+    }
+}
